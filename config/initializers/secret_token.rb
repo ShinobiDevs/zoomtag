73ca7447b398eef2a16b6ebe03cbd87a5b06f2c6
Elad Meidar <elad@eizesus.com>
2013-07-21 15:41:50 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zoomtag::Application.config.secret_key_base = '88cc889af8af2b9eeaeefd562c3558b2a6d31f8020a0984efc87b5d243eb7af64f28151a18c76999d15591a36457df71eed3e188747385d2c556ea7fc51e2830'
