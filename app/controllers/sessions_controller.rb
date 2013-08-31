89555d29052fce0e831504a7652affd243cfaa0f
Adam Zigdon <adam.zigdon@gmail.com>
2013-08-31 18:51:47 +0300

    player create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,20 @@ class SessionsController < Devise::SessionsController
 
   def create
     resource = Player.find_for_database_authentication(facebook_uuid: params[:uuid])
-    
+
     if resource.blank?
       facebook_user = FbGraph::User.fetch :me, access_token: params[:access_token]
+      resource ||= Player.find_for_database_authentication(facebook_uuid: facebook_user.identifier)
 
-      email = facebook_user.email || "guest_#{params[:uuid]}@zoomtag.com"
-      resource = Player.create!(facebook_access_token: facebook_user.access_token.to_s,
-                                facebook_uuid: facebook_user.identifier,
-                                name: facebook_user.name,
-                                profile_picture: facebook_user.picture,
-                                password: Devise.friendly_token[0,20],
-                                email: email)
+      if resource.blank?
+        email = facebook_user.email || "guest_#{facebook_user.identifier}@zoomtag.com"
+        resource = Player.create!(facebook_access_token: facebook_user.access_token.to_s,
+                                  facebook_uuid: facebook_user.identifier,
+                                  name: facebook_user.name,
+                                  profile_picture: facebook_user.picture,
+                                  password: Devise.friendly_token[0,20],
+                                  email: email)
+      end
     end
 
     sign_in(:player, resource)
